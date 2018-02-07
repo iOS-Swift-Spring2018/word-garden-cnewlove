fe943ab2cbfe177a32a1317d727d9fb3caeb94a9
Clark Newlove <newlove@bc.edu>
2018-02-07 12:23:55 -0500

    word garden final

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,48 +9,114 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var userGuessLabel: UILabel!
     @IBOutlet weak var guessedLetterField: UITextField!
     @IBOutlet weak var theGuessCountLabel: UILabel!
     @IBOutlet weak var guessLetterButton: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+    var wordToGuess = "SWIFT"
+    var lettersGuessed = ""
+    let maxNumberOfWrongGuesses = 8
+    var wrongGuessesRemaining = 8
+    var guessCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        formatUserGuessLabel()
         guessLetterButton.isEnabled = false
         playAgainButton.isHidden = true
     }
-
+    
     func updateUIAfterGuess () {
         guessedLetterField.resignFirstResponder()
-        guessedLetterField.text = ""}
+        guessedLetterField.text = ""
+        guessLetterButton.isEnabled = false
+    }
     
+    
+    func formatUserGuessLabel() {
+        var revealedWord = ""
+        lettersGuessed += guessedLetterField.text!
+        
+        for letter in wordToGuess {
+            if lettersGuessed.contains(letter) {
+                revealedWord = revealedWord + " \(letter)"
+            } else {
+                revealedWord += " _"
+            }
+        }
+        revealedWord.removeFirst()
+        userGuessLabel.text = revealedWord
+    }
+    
+    func guessALetter() {
+        formatUserGuessLabel()
+        guessCount += 1
+        
+        // decrements the wrong guesses remaining and shows the image with one less petal
+        let currentLetterGuessed =  guessedLetterField.text!
+        if !wordToGuess.contains(currentLetterGuessed) {
+            wrongGuessesRemaining = wrongGuessesRemaining - 1
+            imageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
+        }
+        let revealedWord = userGuessLabel.text!
+        // stop game if wrongGuessesRemaining = 0
+        if wrongGuessesRemaining == 0 {
+            playAgainButton.isHidden = false
+            guessedLetterField.isEnabled = false
+            guessLetterButton.isEnabled = false
+            theGuessCountLabel.text = "So sorry, you are all out of guesses. Try again?"
+        } else if !revealedWord.contains("_") {
+            // you've won the game
+            playAgainButton.isHidden = false
+            guessedLetterField.isEnabled = false
+            guessLetterButton.isEnabled = false
+            theGuessCountLabel.text = "You've Got It! It took you \(guessCount) guesses to guess the word!"
+        } else {
+            let guess = ( guessCount == 1 ? "guess" : "guesses")
+            theGuessCountLabel.text = "You've made \(guessCount) \(guess)"
+        }
+    }
+    
+
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
         if let letterGuessed = guessedLetterField.text?.last {
             guessedLetterField.text = "\(letterGuessed)"
             guessLetterButton.isEnabled = true
         } else {
             guessLetterButton.isEnabled = false
+        }
     }
-        
     
-}
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
+        guessALetter()
         updateUIAfterGuess()
         
     }
     
+    
+
+    
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
-       updateUIAfterGuess()
+        guessALetter()
+        updateUIAfterGuess()
     }
     
     @IBAction func playAgainButtonPressed(_ sender: Any) {
+        playAgainButton.isHidden = true
+        guessedLetterField.isEnabled = true
+        guessLetterButton.isEnabled = false
+        imageView.image = UIImage(named: "flower8")
+        wrongGuessesRemaining = maxNumberOfWrongGuesses
+        lettersGuessed = ""
+        formatUserGuessLabel()
+        theGuessCountLabel.text = "You've made 0 guesses."
+        guessCount = 0
     }
     
 }
-
 
 
